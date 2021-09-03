182fa46e7903dd67d7cc9188a9722fddaae2d2c4
yiming liu <leviliushmh@gmail.com>
2021-09-03 12:16:42 -0400

    add bsc testnet

--- on-chain/hardhat.config.js
+++ on-chain/hardhat.config.js
@@ -61,6 +61,10 @@ module.exports = {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545",
       accounts: [process.env.bscTestOwnerPK]
     },
+    bsctest: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      accounts: [process.env.bscTestOwnerPK]
+    },
   },
   solidity: {
     compilers: [

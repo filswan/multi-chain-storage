ae8d7a2177bb48d10eea684938a4eec4466f8edd
zhiqiang <zzhao@nbai.io>
2021-10-28 16:51:58 -0400

    modify get lock payment event data

--- routers/EventRouter.go
+++ routers/EventRouter.go
@@ -3,6 +3,8 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"payment-bridge/blockchain/browsersync/goerli"
+	"payment-bridge/blockchain/browsersync/polygon"
 	common "payment-bridge/common"
 	"payment-bridge/common/constants"
 	"payment-bridge/common/errorinfo"
@@ -11,21 +13,20 @@ import (
 )
 
 func EventLogManager(router *gin.RouterGroup) {
-	router.GET("/logs/:contractAddress/:cid", GetEventLogData)
+	router.GET("/logs/lock/:cid", GetLockPaymentEventLogData)
 }
 
-func GetEventLogData(c *gin.Context) {
-	contractAddress := c.Param("contractAddress")
+func GetLockPaymentEventLogData(c *gin.Context) {
 	cid := c.Param("cid")
 	eventListMap := make(map[string]interface{})
-	goerliEventList, err := models.FindEventGoerlis(&models.EventGoerli{ContractAddress: contractAddress, PayloadCid: cid}, "id desc", "", "0")
+	goerliEventList, err := models.FindEventGoerlis(&models.EventGoerli{ContractAddress: goerli.GetConfig().GoerliMainnetNode.PaymentContractAddress, PayloadCid: cid}, "id desc", "", "0")
 	if err != nil {
 		logs.GetLogger().Error(err)
 		c.AbortWithStatusJSON(http.StatusInternalServerError, common.CreateErrorResponse(errorinfo.GET_EVENT_FROM_DB_ERROR_CODE, errorinfo.GET_EVENT_FROM_DB_ERROR_MSG))
 		return
 	}
 	eventListMap[constants.NETWORK_TYPE_GOERLI] = goerliEventList
-	polygonEventList, err := models.FindEventPolygons(&models.EventPolygon{ContractAddress: contractAddress, PayloadCid: cid}, "id desc", "", "0")
+	polygonEventList, err := models.FindEventPolygons(&models.EventPolygon{ContractAddress: polygon.GetConfig().PolygonMainnetNode.PaymentContractAddress, PayloadCid: cid}, "id desc", "", "0")
 	if err != nil {
 		logs.GetLogger().Error(err)
 		c.AbortWithStatusJSON(http.StatusInternalServerError, common.CreateErrorResponse(errorinfo.GET_EVENT_FROM_DB_ERROR_CODE, errorinfo.GET_EVENT_FROM_DB_ERROR_MSG))

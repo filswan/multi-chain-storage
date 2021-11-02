72bbf882326996fd381a2b141a2b45c948b178d8
yiming liu <leviliushmh@gmail.com>
2021-11-02 15:16:53 -0400

    add sign transaction script

--- on-chain/scripts/sign-transaction.js
+++ on-chain/scripts/sign-transaction.js
@@ -4,18 +4,18 @@ const hre = require("hardhat");
 async function main() {
 
   const [signer] = await ethers.getSigners();
-
+ 
   const oracleDAOContractAddress = "0xe3262c0848b0cc5cd43df7139103f1fbf26558cc";
   const contract = await hre.ethers.getContractFactory("FilswanOracle");
   const daoOracleInstance = await contract.attach(oracleDAOContractAddress);
 
-  const cid = "bafykbzacea7nzf7cr4olwxmjjvzszaho2mv34wwbs46yfr7fyegwmxee742b4";
+  const cid = "bafk2bzacedh6keeksywaoa3wjryqzihqixyfekqgfljfosrcoyaj2kozl767s";
   const orderId = "";
-  const dealId = "bafyreifgrtipfm5u6asz7wuf6gx4747ukcx4nuahbzs3npg3y7hqux5sby";
+  const dealId = "0x0000000000000000000000000000000000000000000000000000000000000001";
 
   const paid = "10000000000000000"; // paid filcoins
-  const recipient = "0x6f2B76024196e82D81c8bC5eDe7cff0B0276c9C1";
-  const terms = "2000000000000000";
+  const recipient = "0x591f62C3FDC087dADC8A02dF76fD0a2Bd2168CDF";
+  const terms = "2000000000000000"; 
   const status = true; // true for successful paid, false for failed paid
 
   const tx = await daoOracleInstance.connect(signer).signTransaction(

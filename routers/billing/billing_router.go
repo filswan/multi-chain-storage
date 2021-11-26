acaad92b607120666b379587f72e9e941d10cb10
zhiqiang <zzhao@nbai.io>
2021-11-25 23:59:42 -0500

    fix bub: update volume from network_name to lock_payment_network

--- routers/billing/billing_router.go
+++ routers/billing/billing_router.go
@@ -29,7 +29,7 @@ func UpdateLockPaymentInfoByPayloadCid(c *gin.Context) {
 	}
 
 	payloadCid := c.PostForm("payload_cid")
-	if strings.Trim(payloadCid, " ") != "" {
+	if strings.Trim(payloadCid, " ") == "" {
 		errMsg := "payload_cid can not be null when updating lock payment info"
 		err := errors.New(errMsg)
 		logs.GetLogger().Error(err)
@@ -37,7 +37,7 @@ func UpdateLockPaymentInfoByPayloadCid(c *gin.Context) {
 		return
 	}
 	lockPaymentStatus := c.PostForm("lock_payment_status")
-	if strings.Trim(lockPaymentStatus, " ") != "" {
+	if strings.Trim(lockPaymentStatus, " ") == "" {
 		errMsg := "lock_payment_status can not be null when updating lock payment info"
 		err := errors.New(errMsg)
 		logs.GetLogger().Error(err)
@@ -45,7 +45,7 @@ func UpdateLockPaymentInfoByPayloadCid(c *gin.Context) {
 		return
 	}
 	networkName := c.PostForm("network_name")
-	if strings.Trim(networkName, " ") != "" {
+	if strings.Trim(networkName, " ") == "" {
 		errMsg := "network_name can not be null when updating lock payment info"
 		err := errors.New(errMsg)
 		logs.GetLogger().Error(err)
@@ -53,7 +53,7 @@ func UpdateLockPaymentInfoByPayloadCid(c *gin.Context) {
 		return
 	}
 	lockPaymentTx := c.PostForm("lock_payment_tx")
-	if strings.Trim(lockPaymentTx, " ") != "" {
+	if strings.Trim(lockPaymentTx, " ") == "" {
 		errMsg := "lock_payment_tx can not be null when updating lock payment info"
 		err := errors.New(errMsg)
 		logs.GetLogger().Error(err)
@@ -62,7 +62,7 @@ func UpdateLockPaymentInfoByPayloadCid(c *gin.Context) {
 	}
 	//condition :&models.DealFile{DealCid: "123"}
 	//updateFields: map[string]interface{}{"processing_time": taskT.ProcessingTime, "worker_reward": taskT.WorkerReward}
-	err := models.UpdateDealFile(&models.DealFile{PayloadCid: payloadCid}, map[string]interface{}{"lock_payment_tx": lockPaymentTx, "lock_payment_status": lockPaymentStatus, "networkName": lockPaymentStatus})
+	err := models.UpdateDealFile(&models.DealFile{PayloadCid: payloadCid}, map[string]interface{}{"lock_payment_tx": lockPaymentTx, "lock_payment_status": lockPaymentStatus, "lock_payment_network": lockPaymentStatus})
 	if err != nil {
 		logs.GetLogger().Error(err)
 		c.JSON(http.StatusInternalServerError, common.CreateErrorResponse(errorinfo.UPDATE_DATA_TO_DB_ERROR_CODE, errorinfo.UPDATE_DATA_TO_DB_ERROR_MSG+": update lock payment info to db occurred error"))

c39fcfbc89d92d7511db20d074395a74d6d1ffe2
zhiqiang <rick.zhiqiang.zhao@gmail.com>
2021-12-05 23:44:19 -0500

    support search function

--- routers/storage/storageRouter.go
+++ routers/storage/storageRouter.go
@@ -293,7 +293,9 @@ func GetDealListFromLocal(c *gin.Context) {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, common.CreateErrorResponse(errorinfo.PAGE_NUMBER_OR_SIZE_FORMAT_ERROR_CODE, errorinfo.PAGE_NUMBER_OR_SIZE_FORMAT_ERROR_MSG))
 		return
 	}
-	infoList, err := GetSourceFileAndDealFileInfo(pageSize, strconv.FormatInt(offset, 10), int(userId.(float64)))
+	payloadCid := strings.Trim(URL.Get("payload_cid"), " ")
+	fileName := strings.Trim(URL.Get("file_name"), " ")
+	infoList, err := GetSourceFileAndDealFileInfo(pageSize, strconv.FormatInt(offset, 10), int(userId.(float64)), payloadCid, fileName)
 	if err != nil {
 		logs.GetLogger().Error(err)
 		c.AbortWithStatusJSON(http.StatusInternalServerError, common.CreateErrorResponse(errorinfo.GET_RECORD_lIST_ERROR_CODE, errorinfo.GET_RECORD_lIST_ERROR_MSG+": get source file and deal info from db occurred error"))

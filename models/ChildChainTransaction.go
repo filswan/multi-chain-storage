5da56e8f1907288bcfa03f0f2af0eb884b822c56
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2022-01-05 09:47:52 -0600

    fixed bugs

--- models/ChildChainTransaction.go
+++ models/ChildChainTransaction.go
@@ -8,12 +8,12 @@ import (
 type ChildChainTransaction struct {
 	ID           int64  `json:"id"`
 	TxHashInBsc  string `json:"tx_hash_in_bsc"`
-	TxHashInNbai string `json:"tx_hash_in_nbai""`
+	TxHashInNbai string `json:"tx_hash_in_nbai"`
 	Status       string `json:"status"`
 	BlockNo      uint64 `json:"block_no"` //in nbai
 	RedoTimes    int8   `json:"redo_times"`
 	CreateAt     string `json:"create_at"`
-	UpdateAt     string `json:"create_at"`
+	UpdateAt     string `json:"update_at"`
 }
 
 // FindChildChainTransaction (&ChildChainTransaction{Id: "0xadeaCC802D0f2DFd31bE4Fa7434F15782Fd720ac"},"id desc","10","0")

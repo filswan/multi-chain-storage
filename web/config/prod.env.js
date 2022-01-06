e086ebc7c48a935b1dd34f86618dd68e7eca5741
Charles Cao <ccao@nbai.io>
2022-01-06 18:24:31 -0500

    add docker support

--- web/config/prod.env.js
+++ web/config/prod.env.js
@@ -4,6 +4,7 @@ module.exports = {
   // BASE_API: '"https://mcp.filswan.com/"',
   // BASE_API: '"http://192.168.88.216:5002/"',
   // BASE_PAYMENT_GATEWAY_API: '"http://192.168.88.217:8889/"'
+  // BASE_API: '"https://calibration-api.filswan.com/"',
   BASE_API: '"https://calibration-api.filswan.com/"',
   BASE_PAYMENT_GATEWAY_API: '"https://calibration-mcp-api.filswan.com/"'
 }

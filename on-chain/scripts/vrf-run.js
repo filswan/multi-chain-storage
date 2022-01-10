3c770352b5b56076b943b15b69e6c303bf42f1f8
yiming liu <leviliushmh@gmail.com>
2022-01-10 12:52:12 -0500

    add vrf contract

---
+++ on-chain/scripts/vrf-run.js
@@ -0,0 +1,41 @@
+
+const { ethers, upgrades } = require("hardhat");
+
+const sleep = require('sleep-promise');
+
+const overrides = {
+  gasLimit: 9999999
+}
+
+
+async function main() {
+
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Executing contracts with the account:", deployer.address);
+
+  const vrfContract = await ethers.getContractFactory("SwanVRFConsumer");
+
+  const address = "0x8A61dEff846240ce57a36b0D83391260cD794db1"
+
+  const vrfInstance = await vrfContract.attach(address);
+  const requestId = await vrfInstance.connect(deployer).callStatic.getRandomNumber();
+  const tx = await vrfInstance.connect(deployer).getRandomNumber();
+  await tx.wait();
+  
+  console.log(`random number requestId is: ${requestId}`);
+
+  console.log("Waiting for generating random number (30 seconds)......");
+
+  await sleep(30000); 
+
+  const number = await vrfInstance.requestIdToRandomNumber(requestId);
+  console.log(`random number is:${number}`)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });

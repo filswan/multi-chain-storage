53a7c942448a13b954f52f36ad3689fa29c699f7
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2022-01-11 08:38:58 -0600

    Update version.go

--- common/version.go
+++ common/version.go
@@ -18,7 +18,7 @@ func GetVersion() string {
 }
 
 type HostInfo struct {
-	SwanMinerVersion string `json:"swan_miner_version""`
+	SwanMinerVersion string `json:"swan_miner_version"`
 	OperatingSystem  string `json:"operating_system"`
 	Architecture     string `json:"architecture"`
 	CPUnNumber       int    `json:"cpu_number"`

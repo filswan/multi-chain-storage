e3ea761e7736f0e3b0e86c031e9401687aa8ccdc
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2022-01-13 12:13:22 -0600

    Update EventRouter.go

--- routers/EventRouter.go
+++ routers/EventRouter.go
@@ -23,14 +23,14 @@ func GetLockPaymentEventLogData(c *gin.Context) {
 	goerliEventList, err := models.FindEventGoerlis(&models.EventGoerli{ContractAddress: goerli.GetConfig().GoerliMainnetNode.PaymentContractAddress, PayloadCid: cid}, "id desc", "", "0")
 	if err != nil {
 		logs.GetLogger().Error(err)
-		c.AbortWithStatusJSON(http.StatusInternalServerError, common.CreateErrorResponse(errorinfo.GET_EVENT_FROM_DB_ERROR_CODE, errorinfo.GET_EVENT_FROM_DB_ERROR_MSG))
+		c.AbortWithStatusJSON(http.StatusInternalServerError, common.CreateErrorResponse(errorinfo.GET_EVENT_FROM_DB_ERROR_CODE))
 		return
 	}
 	eventListMap[constants.NETWORK_TYPE_GOERLI] = goerliEventList
 	polygonEventList, err := models.FindEventLockPayment(&models.EventLockPayment{ContractAddress: polygon.GetConfig().PolygonMainnetNode.PaymentContractAddress, PayloadCid: cid}, "id desc", "", "0")
 	if err != nil {
 		logs.GetLogger().Error(err)
-		c.AbortWithStatusJSON(http.StatusInternalServerError, common.CreateErrorResponse(errorinfo.GET_EVENT_FROM_DB_ERROR_CODE, errorinfo.GET_EVENT_FROM_DB_ERROR_MSG))
+		c.AbortWithStatusJSON(http.StatusInternalServerError, common.CreateErrorResponse(errorinfo.GET_EVENT_FROM_DB_ERROR_CODE))
 		return
 	}
 	eventListMap[constants.NETWORK_TYPE_POLYGON] = polygonEventList

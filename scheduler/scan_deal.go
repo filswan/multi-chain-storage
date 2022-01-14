eeb7f7ab0a30103748eb2e908d99153450c93625
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2022-01-14 16:14:49 -0600

    rename

--- scheduler/scan_deal.go
+++ scheduler/scan_deal.go
@@ -6,7 +6,6 @@ import (
 	"payment-bridge/config"
 	"payment-bridge/database"
 	"payment-bridge/models"
-	"strings"
 
 	"github.com/filswan/go-swan-lib/logs"
 
@@ -18,7 +17,7 @@ func ScanDealInfoScheduler() {
 	c := cron.New()
 	err := c.AddFunc(config.GetConfig().ScheduleRule.ScanDealStatusRule, func() {
 		logs.GetLogger().Info("scanning deal info from chain scheduler")
-		err := GetDealInfoByLotusClientAndUpdateInfoToDB()
+		err := scanDeals()
 		if err != nil {
 			logs.GetLogger().Error(err)
 			return
@@ -31,7 +30,7 @@ func ScanDealInfoScheduler() {
 	c.Start()
 }
 
-func GetDealInfoByLotusClientAndUpdateInfoToDB() error {
+func scanDeals() error {
 	whereCondition := "deal_cid != '' and task_uuid != '' and lower(lock_payment_status) not in (lower('" + constants.LOCK_PAYMENT_STATUS_SUCCESS + "'), lower('" + constants.LOCK_PAYMENT_STATUS_REFUNDED + "'))"
 	//" and deal_status not in (" + inList + ")"
 	dealList, err := models.FindDealFileList(whereCondition, "create_at desc", "100", "0")
@@ -40,30 +39,32 @@ func GetDealInfoByLotusClientAndUpdateInfoToDB() error {
 		return err
 	}
 
+	lotusClient, err := lotus.LotusGetClient(config.GetConfig().Lotus.ClientApiUrl, config.GetConfig().Lotus.ClientAccessToken)
+	if err != nil {
+		logs.GetLogger().Error(err)
+		return err
+	}
+
 	for _, deal := range dealList {
-		lotusClient, err := lotus.LotusGetClient(config.GetConfig().Lotus.ClientApiUrl, config.GetConfig().Lotus.ClientAccessToken)
-		if err != nil {
-			logs.GetLogger().Error(err)
-			return err
-		}
 		dealInfo, err := lotusClient.LotusClientGetDealInfo(deal.DealCid)
 		if err != nil {
 			logs.GetLogger().Error(err)
 			return err
 		}
-		paymentStatus := ""
-		if strings.EqualFold(dealInfo.Status, constants.DEAL_STATUS_ACTIVE) {
-			paymentStatus = constants.LOCK_PAYMENT_STATUS_SUCCESS
-		} else if strings.EqualFold(dealInfo.Status, constants.DEAL_STATUS_ERROR) {
-			paymentStatus = constants.LOCK_PAYMENT_STATUS_REFUNDED
-		} else {
-			paymentStatus = constants.LOCK_PAYMENT_STATUS_PROCESSING
+
+		switch dealInfo.Status {
+		case constants.DEAL_STATUS_ACTIVE:
+			deal.LockPaymentStatus = constants.LOCK_PAYMENT_STATUS_SUCCESS
+		case constants.DEAL_STATUS_ERROR:
+			deal.LockPaymentStatus = constants.LOCK_PAYMENT_STATUS_REFUNDED
+		default:
+			deal.LockPaymentStatus = constants.LOCK_PAYMENT_STATUS_PROCESSING
 		}
+
 		deal.Verified = dealInfo.Verified
 		deal.DealStatus = dealInfo.Status
 		deal.DealId = dealInfo.DealId
 		deal.Cost = dealInfo.CostComputed
-		deal.LockPaymentStatus = paymentStatus
 		deal.UpdateAt = utils.GetCurrentUtcMilliSecond()
 		err = database.SaveOne(deal)
 		if err != nil {

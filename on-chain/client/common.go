9c0a8e48e35592235fecd93ac6110810ab604851
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2022-01-29 19:16:55 -0600

    refactor

--- on-chain/client/common.go
+++ on-chain/client/common.go
@@ -6,13 +6,8 @@ import (
 	"math/big"
 	"os"
 	"payment-bridge/blockchain/browsersync/scanlockpayment/polygon"
-	"payment-bridge/common/constants"
-	"payment-bridge/common/utils"
 	"payment-bridge/config"
-	"payment-bridge/database"
-	"payment-bridge/models"
 	"payment-bridge/on-chain/goBind"
-	"strconv"
 	"strings"
 
 	"github.com/filswan/go-swan-lib/logs"
@@ -142,66 +137,4 @@ func GetContractAbi() (*abi.ABI, error) {
 	}
 
 	return &contractAbi, nil
-}
-
-func GetPaymentInfo(srcFilePayloadCid string) (*models.EventLockPayment, error) {
-	swanPaymentSession, err := GetPaymentSession()
-	if err != nil {
-		logs.GetLogger().Error(err)
-		return nil, err
-	}
-
-	paymentInfo, err := swanPaymentSession.GetLockedPaymentInfo(srcFilePayloadCid)
-	if err != nil {
-		logs.GetLogger().Error(err)
-		return nil, err
-	}
-
-	var event *models.EventLockPayment
-	if paymentInfo.IsExisted {
-		event = new(models.EventLockPayment)
-		event.TokenAddress = paymentInfo.Token.Hex()
-		event.AddressFrom = paymentInfo.Owner.String()
-		event.AddressTo = paymentInfo.Recipient.String()
-		event.LockedFee = paymentInfo.LockedFee.String()
-		event.PayloadCid = srcFilePayloadCid
-		event.LockPaymentTime = strconv.FormatInt(utils.GetCurrentUtcMilliSecond(), 10)
-		usdcCoinId, err := models.FindCoinIdByUUID(constants.COIN_TYPE_USDC_ON_POLYGON_UUID)
-		if err != nil {
-			logs.GetLogger().Error(err)
-		} else {
-			event.CoinId = usdcCoinId
-		}
-
-		networkId, err := models.FindNetworkIdByUUID(constants.NETWORK_TYPE_POLYGON_UUID)
-		if err != nil {
-			logs.GetLogger().Error(err)
-		} else {
-			event.NetworkId = networkId
-		}
-
-		err = database.SaveOne(event)
-		if err != nil {
-			logs.GetLogger().Error(err)
-			return nil, err
-		}
-
-		dealFiles, err := models.GetDealFileBySourceFilePayloadCid(srcFilePayloadCid)
-		if err != nil {
-			logs.GetLogger().Error(err)
-			return nil, err
-		}
-
-		if len(dealFiles) > 0 {
-			dealFileId := dealFiles[0].ID
-
-			err := models.UpdateDealFileLockPaymentStatus(dealFileId, constants.LOCK_PAYMENT_STATUS_PROCESSING)
-			if err != nil {
-				logs.GetLogger().Error(err)
-				return nil, err
-			}
-		}
-	}
-
-	return event, nil
 }

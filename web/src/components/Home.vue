d4c7823c553c0f6648ea7f0dad06fbf78ad9ac32
xieyi <yxie@nbai.io>
2022-03-04 17:19:50 +0800

    Adjust billing search

--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -111,7 +111,7 @@ export default {
                 window.location.reload();
             }
         }
-        console.log('update time: 2022-02-28')
+        console.log('update time: 2022-03-04')
     }
 };
 </script>

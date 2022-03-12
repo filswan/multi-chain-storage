6f789776350cc675e72a0af8f9c3e51c19fcfbb0
xieyi <yxie@nbai.io>
2022-03-12 17:19:45 +0800

    Modify refresh upload page

--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -111,7 +111,7 @@ export default {
                 window.location.reload();
             }
         }
-        console.log('update time: 2022-03-11')
+        console.log('update time: 2022-03-12')
     }
 };
 </script>

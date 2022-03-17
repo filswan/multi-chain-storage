31b1623ddc69177082ebe31cf1cbacc0084c197e
xieyi <yxie@nbai.io>
2022-03-17 16:09:12 +0800

    Show exact file size

--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -111,7 +111,7 @@ export default {
                 window.location.reload();
             }
         }
-        console.log('update time: 2022-03-12')
+        console.log('update time: 2022-03-17')
     }
 };
 </script>

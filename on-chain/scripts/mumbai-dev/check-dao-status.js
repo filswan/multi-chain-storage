02dc3bbcbfb5088fd8382d875d12744e6c0bbb4d
yiming liu <leviliushmh@gmail.com>
2022-03-18 23:51:41 -0400

    add script for testing contract

---
+++ on-chain/scripts/mumbai-dev/check-dao-status.js
@@ -0,0 +1,37 @@
+
+const hre = require("hardhat");
+
+async function main() {
+
+ 
+  const oracleDAOContractAddress = "0x6f83DA2C5f1C5AAC259aD8d817Bb92c2D863F74c";
+
+  const addressList = [
+    "0x800210CfB747992790245eA878D32F188d01a03A",
+    "0x05856015d07F3E24936B7D20cB3CcfCa3D34B41d",
+    "0x6f2B76024196e82D81c8bC5eDe7cff0B0276c9C1"
+  ];
+
+  const [signer] = await ethers.getSigners();
+
+  const contract = await hre.ethers.getContractFactory("FilswanOracle");
+  const daoOracleInstance = await contract.attach(oracleDAOContractAddress);
+
+  const result = await daoOracleInstance.getCarPaymentVotes('87204','0xc4fcaAdCb0b00a9501e56215c37B10fAF9e79c0a');
+  console.log(result);
+
+  // const hasRole = await daoOracleInstance.connect(signer).hasRole(
+  //   role,
+  //   addressList[0]);
+
+  // console.log(hasRole);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });

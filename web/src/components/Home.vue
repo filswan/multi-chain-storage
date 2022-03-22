0daec3f3cfdf01d5d5514518b425be5813a8358c
xieyi <yxie@nbai.io>
2022-03-22 16:12:26 +0800

    Modify QA-2022.3.22 documents

--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -111,7 +111,7 @@ export default {
                 window.location.reload();
             }
         }
-        console.log('update time: 2022-03-17')
+        console.log('update time: 2022-03-22')
     }
 };
 </script>

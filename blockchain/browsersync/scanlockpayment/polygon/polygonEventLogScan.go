54bd0c08cb8d0a5e7ed93d453bd7f52d22c16ffe
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2022-04-05 11:12:38 -0600

    Update polygonEventLogScan.go

--- blockchain/browsersync/scanlockpayment/polygon/polygonEventLogScan.go
+++ blockchain/browsersync/scanlockpayment/polygon/polygonEventLogScan.go
@@ -47,63 +47,63 @@ func ScanEventFromChainAndSaveDataToDbForPolygon() {
 		blockScanRecordList, err := blockScanRecord.FindLastCurrentBlockNumber(whereCondition)
 		if err != nil {
 			logs.GetLogger().Error(err)
-			startScanBlockNo = GetConfig().PolygonMainnetNode.StartFromBlockNo
-		}
-		if len(blockScanRecordList) > 0 {
-			if blockScanRecordList[0].LastCurrentBlockNumber <= blockNoCurrent.Int64() {
-				startScanBlockNo = blockScanRecordList[0].LastCurrentBlockNumber
-			} else {
-				startScanBlockNo = GetConfig().PolygonMainnetNode.StartFromBlockNo
-			}
-			blockScanRecord.ID = blockScanRecordList[0].ID
-		}
-
-		for {
-			start := startScanBlockNo
-			end := start + GetConfig().PolygonMainnetNode.ScanStep
-			if startScanBlockNo > blockNoCurrent.Int64() {
-				break
-			}
-			err = ScanPolygonLockPaymentEventFromChainAndSaveEventLogData(start-1, end+1)
-			if err != nil {
-				logs.GetLogger().Error(err)
-				continue
+		} else {
+			if len(blockScanRecordList) > 0 {
+				if blockScanRecordList[0].LastCurrentBlockNumber <= blockNoCurrent.Int64() {
+					startScanBlockNo = blockScanRecordList[0].LastCurrentBlockNumber
+				} else {
+					startScanBlockNo = GetConfig().PolygonMainnetNode.StartFromBlockNo
+				}
+				blockScanRecord.ID = blockScanRecordList[0].ID
 			}
 
-			err = ScanDaoEventFromChainAndSaveEventLogData(start-1, end+1)
-			if err != nil {
-				logs.GetLogger().Error(err)
-				continue
-			}
+			for {
+				start := startScanBlockNo
+				end := start + GetConfig().PolygonMainnetNode.ScanStep
+				if startScanBlockNo > blockNoCurrent.Int64() {
+					break
+				}
+				err = ScanPolygonLockPaymentEventFromChainAndSaveEventLogData(start-1, end+1)
+				if err != nil {
+					logs.GetLogger().Error(err)
+					continue
+				}
 
-			err = ScanPolygonExpirePaymentEventFromChainAndSaveEventLogData(start-1, end+1)
-			if err != nil {
-				logs.GetLogger().Error(err)
-				continue
-			}
+				err = ScanDaoEventFromChainAndSaveEventLogData(start-1, end+1)
+				if err != nil {
+					logs.GetLogger().Error(err)
+					continue
+				}
 
-			if end >= blockNoCurrent.Int64() {
-				blockScanRecord.LastCurrentBlockNumber = blockNoCurrent.Int64()
-			} else {
-				blockScanRecord.LastCurrentBlockNumber = end
-			}
-			networkId, err := models2.FindNetworkIdByUUID(constants.NETWORK_TYPE_POLYGON_UUID)
-			if err != nil {
-				logs.GetLogger().Error(err)
-			} else {
-				blockScanRecord.NetworkId = networkId
-			}
-			blockScanRecord.UpdateAt = strconv.FormatInt(utils.GetEpochInMillis(), 10)
+				err = ScanPolygonExpirePaymentEventFromChainAndSaveEventLogData(start-1, end+1)
+				if err != nil {
+					logs.GetLogger().Error(err)
+					continue
+				}
 
-			err = database.SaveOne(blockScanRecord)
-			if err != nil {
-				logs.GetLogger().Error(err)
-				continue
-			}
-			start = end
-			startScanBlockNo = end
-			if end >= blockNoCurrent.Int64() {
-				break
+				if end >= blockNoCurrent.Int64() {
+					blockScanRecord.LastCurrentBlockNumber = blockNoCurrent.Int64()
+				} else {
+					blockScanRecord.LastCurrentBlockNumber = end
+				}
+				networkId, err := models2.FindNetworkIdByUUID(constants.NETWORK_TYPE_POLYGON_UUID)
+				if err != nil {
+					logs.GetLogger().Error(err)
+				} else {
+					blockScanRecord.NetworkId = networkId
+				}
+				blockScanRecord.UpdateAt = strconv.FormatInt(utils.GetEpochInMillis(), 10)
+
+				err = database.SaveOne(blockScanRecord)
+				if err != nil {
+					logs.GetLogger().Error(err)
+					continue
+				}
+				start = end
+				startScanBlockNo = end
+				if end >= blockNoCurrent.Int64() {
+					break
+				}
 			}
 		}
 

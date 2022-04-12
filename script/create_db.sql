74e7bd54d09cd22eabe23635709254af5991735a
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2022-04-12 14:35:48 -0600

    Update create_db.sql

--- script/create_db.sql
+++ script/create_db.sql
@@ -17,31 +17,29 @@ create table network (
     id            bigint        not null auto_increment,
     name          varchar(100)  not null,
     rpc_url       varchar(1000) not null,
-    description   text
+    description   text,
     create_at     bigint        not null,
     update_at     bigint        not null,
     primary key pk_network(id),
     constraint un_network_name unique(name)
 );
 
-
 create table coin (
     id            bigint       not null auto_increment,
     name          varchar(100) not null,
     network_id    bigint       not null,
-    description   text
+    description   text,
     create_at     bigint       not null,
     update_at     bigint       not null,
     primary key pk_coin(id),
     constraint un_coin_name unique(name),
     constraint fk_coin_network_id foreign key (network_id) references network(id)
-)
+);
 
 create table source_file (
     id            bigint        not null auto_increment,
     file_type     int           not null,  #--0:normal file, 1:mint file
     status        varchar(100)  not null,
-    uuid          varchar(100)  not null,
     payload_cid   varchar(100)  not null,
     resource_uri  varchar(1000) not null,
     ipfs_url      varchar(1000) not null,
@@ -52,7 +50,6 @@ create table source_file (
     update_at     bigint        not null,
     primary key pk_source_file(id),
     constraint un_source_file_resource_uri unique(resource_uri),
-    constraint un_source_file_uuid_payload_cid unique(uuid,payload_cid),
     constraint fk_source_file_wallet_id foreign key (wallet_id) references wallet(id)
 );
 
@@ -61,19 +58,21 @@ create table source_file_mint_info (
     source_file_id bigint        not null,
     nft_tx_hash    varchar(100)  not null,
     mint_address   varchar(100)  not null,
-    token_id ...
+    token_id       varchar(100)  not null,
     primary key pk_source_file_mint_info(id),
     constraint fk_source_file_mint_info_source_file_id foreign key (source_file_id) references source_file(id)
-)
+);
 
 create table source_file_upload_history (
     id             bigint        not null auto_increment,
     source_file_id bigint        not null,
     file_name      varchar(200)  not null,
+    uuid           varchar(100)  not null,
     wallet_id      bigint        not null,
     create_at      bigint        not null,
     update_at      bigint        not null,
     primary key pk_source_file_upload_history(id),
+    constraint un_source_file_upload_history unique(source_file_id,uuid),
     constraint fk_source_file_upload_history_wallet_id foreign key (wallet_id) references wallet(id)
 );
 
@@ -142,7 +141,6 @@ create table transaction_source_file (
     constraint fk_transaction_source_file_wallet_id_to foreign key (wallet_id_to) references wallet(id)
 );
 
-
 create table transaction_offline_deal (
     id               bigint        not null auto_increment,
     offline_deal_id  bigint        not null,
@@ -154,7 +152,5 @@ create table transaction_offline_deal (
     constraint fk_transaction_offline_deal_offline_deal_id foreign key (offline_deal_id) references offline_deal(id),
     constraint fk_transaction_offline_deal_wallet_id_from foreign key (wallet_id_from) references wallet(id),
     constraint fk_transaction_offline_deal_wallet_id_to foreign key (wallet_id_to) references wallet(id)
-)
-
-
+);
 

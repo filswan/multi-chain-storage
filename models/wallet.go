48da39b7f92ebf69f5aea1f08379bb05aaa2581b
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2022-04-13 08:27:04 -0600

    Create wallet.go

---
+++ models/wallet.go
@@ -0,0 +1,33 @@
+package models
+
+import (
+	"fmt"
+	"multi-chain-storage/database"
+
+	"github.com/filswan/go-swan-lib/logs"
+)
+
+type Wallet struct {
+	ID       int64  `json:"id"`
+	Address  string `json:"address"`
+	Type     string `json:"type"`
+	CreateAt int64  `json:"create_at"`
+}
+
+func GeWalletByAddress(address string, walletType int) (*Wallet, error) {
+	var wallets []*Wallet
+	err := database.GetDB().Where("address=? and type=?", address, walletType).Find(&wallets).Error
+	if err != nil {
+		logs.GetLogger().Error(err)
+		return nil, err
+	}
+
+	if len(wallets) > 0 {
+		return wallets[0], nil
+	}
+
+	err = fmt.Errorf("no wallet for address:%s", address)
+	logs.GetLogger().Error(err)
+
+	return nil, err
+}

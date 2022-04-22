073bc251e8b3ae0e1e2c87d0c3b5fb3d5f9db145
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2022-04-22 14:44:03 -0600

    Update constants.go

--- common/constants/constants.go
+++ common/constants/constants.go
@@ -14,7 +14,6 @@ const (
 	HTTP_CODE_400_BAD_REQUEST           = 400 //http.StatusBadRequest
 	HTTP_CODE_401_UNAUTHORIZED          = 401 //http.StatusUnauthorized
 	HTTP_CODE_500_INTERNAL_SERVER_ERROR = 500 //http.StatusInternalServerError
-	HTTP_REQUEST_HEADER_AUTHRORIZATION  = "Authorization"
 
 	URL_HOST_GET_COMMON      = "/common"
 	URL_HOST_GET_HOST_INFO   = "/host/info"

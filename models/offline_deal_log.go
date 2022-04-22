e2f27e8101bace2d2ae2b33dad405c816458c5ae
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2022-04-22 12:31:36 -0600

    Update offline_deal_log.go

--- models/offline_deal_log.go
+++ models/offline_deal_log.go
@@ -1,9 +1,26 @@
 package models
 
+import (
+	"multi-chain-storage/database"
+
+	"github.com/filswan/go-swan-lib/logs"
+)
+
 type OfflineDealLog struct {
 	Id             int64  `json:"id"`
 	OfflineDealId  int64  `json:"offline_deal_id"`
 	OnChainStatus  string `json:"on_chain_status"`
 	OnChainMessage string `json:"on_chain_message"`
 	CreateAt       int64  `json:"create_at"`
+}
+
+func GetOfflineDealLogsByDealCid(dealCid string) ([]*OfflineDealLog, error) {
+	var offlineDealLogs []*OfflineDealLog
+	err := database.GetDB().Where("deal_cid=?", dealCid).Order("id desc").Find(&offlineDealLogs).Error
+	if err != nil {
+		logs.GetLogger().Error(err)
+		return nil, err
+	}
+
+	return offlineDealLogs, nil
 }

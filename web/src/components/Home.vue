2394dd5665b1c38a5ce6fd52434ba3fd93ede977
xieyi <yxie@nbai.io>
2022-05-19 16:27:26 +0800

    Fix search issues

--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -101,7 +101,7 @@ export default {
                 window.location.reload();
             }
         }
-        console.log('update time: 2022-05-18')
+        console.log('update time: 2022-05-19')
     }
 };
 </script>

bf66e813383f6101ae0b0281504c04584029e011
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2022-06-06 09:05:04 -0600

    Update build_from_source.sh

--- build_from_source.sh
+++ build_from_source.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
-CONF_FILE_DIR_DEST=${HOME}/.swan/mcs
-mkdir -p ${CONF_FILE_DIR_DEST}
+CONF_FILE_DIR=${HOME}/.swan/mcs
+mkdir -p ${CONF_FILE_DIR}
 
-CONF_FILE_NAMES=(
-    env
-    config.toml
-    )
+CONF_FILE_PATH=${CONF_FILE_DIR}/config.toml
+if [ -f "${CONF_FILE_PATH}" ]; then
+    echo "${CONF_FILE_PATH} exists"
+else
+    cp ./config/config.toml.example $CONF_FILE_PATH
+    echo "${CONF_FILE_PATH} created"
+fi
 
-for CONF_FILE_NAME in ${CONF_FILE_NAMES[@]}; do
-    CONF_FILE_PATH_SRC=./config/${CONF_FILE_NAME}.example
-    CONF_FILE_PATH_DEST=${CONF_FILE_DIR_DEST}/${CONF_FILE_NAME}
+ENV_FILE_PATH=${CONF_FILE_DIR}/.env
+if [ -f "${ENV_FILE_PATH}" ]; then
+    echo "${ENV_FILE_PATH} exists"
+else
+    cp ./config/env.example $ENV_FILE_PATH
+    echo "${ENV_FILE_PATH} created"
+fi
 
-    if [ -f "$CONF_FILE_PATH_DEST" ]; then
-        echo "$CONF_FILE_PATH_DEST exists"
-    else
-        cp $CONF_FILE_PATH_SRC $CONF_FILE_PATH_DEST
-        echo "copied $CONF_FILE_PATH_SRC to $CONF_FILE_PATH_DEST"
-    fi
-done
-
-mv ${CONF_FILE_DIR_DEST}/env ${CONF_FILE_DIR_DEST}/.env
 cp ./on-chain/contracts/abi/SwanPayment.json $CONF_FILE_DIR_DEST/SwanPayment.json
 
 git submodule update --init --recursive

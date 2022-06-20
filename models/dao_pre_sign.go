95881e682e81fa9bdefbf84e839f932e3b0c3dd2
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2022-06-20 14:46:21 -0600

    Update dao_pre_sign.go

--- models/dao_pre_sign.go
+++ models/dao_pre_sign.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"multi-chain-storage/database"
+
+	"github.com/filswan/go-swan-lib/logs"
+)
+
 type DaoPreSign struct {
 	Id                       int64  `json:"id"`
 	OfflineDealId            int64  `json:"offline_deal_id"`
@@ -10,4 +16,25 @@ type DaoPreSign struct {
 	SourceFileUploadCntSign  int    `json:"source_file_upload_cnt_sign"`
 	CreateAt                 int64  `json:"create_at"`
 	UpdateAt                 int64  `json:"update_at"`
+}
+
+func GetDaoPreSignSourceFileUploadCntSign(offlineDealId int64) (*int, error) {
+	var daoPreSigns []*DaoPreSign
+	sql := "select count(*) source_file_upload_cnt_sign from (\n" +
+		"select b.source_file_upload_id,count(*) from dao_signature a, dao_signature_source_file_upload b\n" +
+		"where a.id=b.dao_signature_id and a.offline_deal_id=?\n" +
+		"group by b.source_file_upload_id) a"
+	err := database.GetDB().Raw(sql, offlineDealId).Scan(&daoPreSigns).Error
+
+	if err != nil {
+		logs.GetLogger().Error(err)
+		return nil, err
+	}
+
+	var sourceFileUploadCntSign int = 0
+	if len(daoPreSigns) > 0 {
+		sourceFileUploadCntSign = daoPreSigns[0].SourceFileUploadCntSign
+	}
+
+	return &sourceFileUploadCntSign, nil
 }

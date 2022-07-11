b28c05544fc5a1dd23f03b6820956cc854b097fe
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2022-07-11 12:46:21 -0400

    when scan refund info, when no offline deals, set to refunded

--- models/offline_deal.go
+++ models/offline_deal.go
@@ -132,6 +132,21 @@ func GetOfflineDealsNotSuccessBySourceFileUploadId(sourceFileUploadId int64) ([]
 	return offlineDeals, nil
 }
 
+func GetOfflineDealsBySourceFileUploadId(sourceFileUploadId int64) ([]*OfflineDeal, error) {
+	var offlineDeals []*OfflineDeal
+	sql := "select c.* from source_file_upload a, car_file_source b, offline_deal c\n" +
+		"where a.id=b.source_file_upload_id and b.car_file_id=c.car_file_id\n" +
+		"  and a.id=?"
+	err := database.GetDB().Raw(sql, sourceFileUploadId).Scan(&offlineDeals).Error
+
+	if err != nil {
+		logs.GetLogger().Error(err)
+		return nil, err
+	}
+
+	return offlineDeals, nil
+}
+
 func GetOfflineDealsByCarFileId(carFileId int64) ([]*OfflineDeal, error) {
 	var offlineDeals []*OfflineDeal
 	err := database.GetDB().Where("car_file_id=?", carFileId).Find(&offlineDeals).Error

c095788df8e7ecffbd85f9449bea096ef514da0c
DoraNebula <90062637+DoraNebula@users.noreply.github.com>
2022-07-14 10:15:51 -0400

    Update common.go

--- common/utils/common.go
+++ common/utils/common.go
@@ -1,18 +1,15 @@
 package utils
 
 import (
-	"crypto/sha1"
+	"crypto/md5"
 	"fmt"
 
 	"github.com/filswan/go-swan-lib/logs"
 )
 
 func GenerateHash(str string) string {
-	h := sha1.New()
-	h.Write([]byte(str))
-	bs := h.Sum(nil)
-
-	hashStrHex := fmt.Sprintf("%x\n", bs)
-	logs.GetLogger().Info(hashStrHex)
-	return hashStrHex
+	var hash = md5.Sum([]byte(str))
+	hashStr := fmt.Sprintf("%x", hash)
+	logs.GetLogger().Info(hashStr)
+	return hashStr
 }

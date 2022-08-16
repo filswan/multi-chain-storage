3e5b3e30deaa78ab5ecfbbe41af2eba304a70ec3
xieyi <yxie@nbai.io>
2022-08-16 17:52:56 +0800

    Update free upload UI

--- web/src/App.vue
+++ web/src/App.vue
@@ -17,11 +17,15 @@ export default {
   filter: invert(1) hue-rotate(180deg);
   .el-button, .createTask a, .logged_in .info, .metamaskLogin, .metamask, .sidebar, svg, img.resno,
   .el-popover.el-popper, #billing .form .form_top .search .el-input .el-input__icon,
-  #dealManagement .form .form_table .el-table tr th .cell i, .filter_status{
+  #dealManagement .form .form_table .el-table tr th .cell i, .filter_status,
+  .header-right .progress{
     filter: invert(1) hue-rotate(180deg);
   }
   .color, .resno, .sidebar img{
     filter: invert(0) hue-rotate(0deg);
+  }
+  .header-right .progress .tip{
+    color: #fff;
   }
 }
 </style>

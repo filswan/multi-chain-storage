47538049fafc59801bb08a7f1da4b9b8b67293db
xieyi <yxie@nbai.io>
2022-08-25 20:02:08 +0800

    .

--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -112,7 +112,7 @@ export default {
             }
         }
         this.init()
-        console.log('update time: 2022-08-25')
+        console.log('update time: 2022-08-25') 
     }
 };
 </script>

bbd4124175c688bc72aa88482953047a16770f0b
rykci <yuen.ricky@hotmail.com>
2022-11-18 09:45:07 -0500

    rename oracle contract

--- on-chain/contracts/FilswanOracleV2.sol
+++ on-chain/contracts/FilswanOracleV2.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
-import "../FilinkConsumer.sol";
+import "./FilinkConsumer.sol";
 
 contract FilswanOracleV2 is OwnableUpgradeable, AccessControlUpgradeable {
     bytes32 public constant DAO_ROLE = keccak256("DAO_ROLE");

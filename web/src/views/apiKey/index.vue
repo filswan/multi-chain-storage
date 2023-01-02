94b341e94896d161794de2846fbe1c3082d42f0d
PlutoNbai <pluto.zhang@nbai.io>
2023-01-02 13:31:22 -0500

    Generate APIkey API Change

--- web/src/views/apiKey/index.vue
+++ web/src/views/apiKey/index.vue
@@ -97,7 +97,7 @@ export default {
       const params = {
         'valid_days': day
       }
-      const apiKeyRes = await that.$commonFun.sendRequest(`${process.env.BASE_PAYMENT_GATEWAY_API}api/v1/user/generate_api_key?${QS.stringify(params)}`, 'post')
+      const apiKeyRes = await that.$commonFun.sendRequest(`${process.env.BASE_PAYMENT_GATEWAY_API}api/v1/user/generate_api_key?${QS.stringify(params)}`, 'get')
       if (!apiKeyRes || apiKeyRes.status !== 'success') {
         that.$message.error(apiKeyRes.message ? apiKeyRes.message : 'Fail')
         that.createLoad = false

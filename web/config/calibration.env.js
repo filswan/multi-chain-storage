edc2ef9d9b879e81ff63f16ae31a4788cc952ca0
yxie@nbai.io <yxie@nbai.io>
2023-06-15 08:10:01 +0800

    Fix payment

--- web/config/calibration.env.js
+++ web/config/calibration.env.js
@@ -5,7 +5,7 @@ module.exports = {
   BASE_URL: '"https://calibration-mcs.filswan.com"',
   BASE_PAYMENT_GATEWAY_API: '"https://calibration-mcs-api.filswan.com/"',
   BASE_PAYMENT_GATEWAY_BSC_API: '"https://calibration-mcs-bsc.filswan.com/"',
-  BASE_PAYMENT_GATEWAY_POLYGON_API: '"https://api.multichain.storage/"',
+  BASE_PAYMENT_GATEWAY_POLYGON_API: '"https://calibration-mcs-api.filswan.com/"',
   BASE_MAINNET_ADDRESS: '"https://filscan.io/tipset/dsn-detail"',
   BASE_CALIBRATION_ADDRESS: '"https://calibration.filscan.io/tipset/dsn-detail"',
   BASE_MUMBAI_ADDRESS: '"https://mumbai.polygonscan.com/"',
